83b276a6f7d5610dd47d76fee8b43b0d7a121599
EvaGraceSmith <smithevagrace@gmail.com>
2023-06-30 02:38:02 -0500

    using starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import {React,useContext} from 'react';
 import Todo from './Components/Todo';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-// import SettingsForm from './Components/SettingsForm';
 import SettingsPage from './Components/Settings/SettingsPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import {HeaderSimple} from './Components/Header/index';
-
+import { SettingsContext } from './Context/Settings';
+import Auth from './Components/Auth';
 
 export default function App() {
+  const { title, email } = useContext(SettingsContext);
   return (
 
     <BrowserRouter>
       <Header />
+      <Auth capability="read">
+        <p>I can read!</p>
+      </Auth>
       <Routes>
         <Route path="/" element={<Todo />} />
         <Route path="/settings" element={<SettingsPage />} />
--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import SettingsProvider from './Context/Settings';
 import { MantineProvider } from '@mantine/core';
+import ModeProvider from './Context/Mode';
+import AuthProvider from './Context/Auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
+    <AuthProvider>
+        <ModeProvider>
+          <SettingsProvider>
+            <App />
+          </SettingsProvider>
+        </ModeProvider>
+      </AuthProvider>
     </MantineProvider>
   </React.StrictMode>
 );
